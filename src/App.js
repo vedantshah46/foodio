49afb8318b55843765748ac525c148a95476f432
Yatri-patel <yatripatel02@gmail.com>
2024-08-02 14:51:01 +0530

    link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { HomePage } from './MyComponents/HomePage/HomePage';
+import NavBar from './MyComponents/NavBar/NavBar';
 
 function App() {
   return (
-   <HomePage/>
-   
+    <>
+    <NavBar/>
+      <HomePage />
+    </>
   );
 }
 
---
+++ src/MyComponents/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Images
+import LogoCap from './Images/brandLogoCap.svg';
+
+const NavBar = () => {
+    return (
+        <>
+            <h1>cvb</h1>
+            <img src={LogoCap} alt="" className='bg-dark'/>
+
+        </>
+    )
+}
+
+export default NavBar

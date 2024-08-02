32bb854b5ed7e036b9b0f3cfe6504d3b961ed139
Yatri-patel <yatripatel02@gmail.com>
2024-08-02 14:07:33 +0530

    yatri09z

--- src/MyComponents/HomePage/HomePage.jsx
+++ src/MyComponents/HomePage/HomePage.jsx
@@ -3,17 +3,13 @@ import React from 'react'
 export const HomePage = () => {
   return (
     <>
-<div className="sectionOne">
-  <h1>dfghjk</h1>
-</div>
+      <div className="sectionOne">
+        <h1>dfghjk</h1>
 
+      </div>
+      <div className="sectionTow">
+        <h1>Vedant</h1>
+      </div>
     </>
   )
-    return (
-        <>
-            <div className="sectionTow">
-                <h1>Vedant</h1>
-            </div>
-        </>
-    )
 }

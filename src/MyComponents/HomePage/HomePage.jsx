e4145b1ab2c199e4fb90d59efaf6135074345fbc
vedantshah46 <vedantshah022@gmail.com>
2024-08-03 15:23:57 +0530

    02-08 Vedant

--- src/MyComponents/HomePage/HomePage.jsx
+++ src/MyComponents/HomePage/HomePage.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import homePageHeroSectionBg from './Images/homePageHeroSectionBg.png';
+import './HomePage.css';
 
 export const HomePage = () => {
-  return (
-    <>
-      <div className="sectionOne">
-        <h1>dfghjk</h1>
+    return (
+        <>
+            <div className="sectionOne">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="hpSectionOneLeft">
+                                
+                                <div className="hpSectionOneLeftText">
+                                    <p>Best Taste</p>
+                                    <h1>Healthy & Tasty food.</h1>
+                                    <h6>Delight in the world of delectable, healthful cuisine that thrills your taste buds while feeding your body - welcome to the universe of Nutritious & Tasty food!</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <div className="hpSectionOneRight">
 
-      </div>
-      <div className="sectionTow">
-        <h1>Vedant</h1>
-      </div>
-    </>
-  )
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }

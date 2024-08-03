04ef238ba6841a22178413dcffdf5f55230884b6
Yatri-patel <yatripatel02@gmail.com>
2024-08-03 15:24:01 +0530

    02-08-yatri

--- src/MyComponents/NavBar/NavBar.jsx
+++ src/MyComponents/NavBar/NavBar.jsx
@@ -1,16 +1,92 @@
-import React from 'react'
+// import React from 'react'
+
+// // Images
+// import LogoCap from './Images/brandLogoCap.svg';
+
+// const NavBar = () => {
+//     return (
+//         <>
+//             {/* <nav class="navbar bg-body-#1b1c1f"> */}
+
+
+//             <nav class="navbar navbar-expand-lg bg-dark">
+//                 <div class="container">
+//                     <a class="navbar-brand" href="#">
+//                         <img src={LogoCap} alt="" />
+//                     </a>
+                 
+//                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//                         <span class="navbar-toggler-icon"></span>
+//                     </button>
+//                     <div class="collapse navbar-collapse" id="navbarNavDropdown">
+//                         <ul class="navbar-nav">
+
+//                             <li class="nav-item">
+//                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
+//                             </li>
+
+//                             <li class="nav-item">
+//                                 <a class="nav-link" href="#">About</a>
+//                             </li>
+
+//                             <li class="nav-item">
+//                                 <a class="nav-link" href="#">Menu</a>
+//                             </li>
+
+//                             <li class="nav-item">
+//                                 <a class="nav-link" href="#">Recipes</a>
+//                             </li>
+//                         </ul>
+//                     </div>
+//                     <button class="btn btn-outline-success" type="submit">Reserve Now</button>
+//                 </div>
+//             </nav>
+//             {/* </nav> */}
+
+//         </>
+//     )
+// }
+
+// export default NavBar
+
+import React from 'react';
+import './NavBar.css'; // Import your custom CSS file
 
 // Images
 import LogoCap from './Images/brandLogoCap.svg';
 
 const NavBar = () => {
     return (
-        <>
-            <h1>cvb</h1>
-            <img src={LogoCap} alt="" className='bg-dark'/>
-
-        </>
-    )
+        <nav className="navbar navbar-expand-lg bg-dark">
+            <div className="container">
+                
+                <a className="navbar-brand" href="#">
+                    <img src={LogoCap} alt="Brand Logo" />
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav ">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Menu</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Recipes</a>
+                        </li>
+                    </ul>
+                </div>
+                <button className="btn btn-danger order-btn" type="button">Reserve now</button>
+            </div>
+        </nav>
+    );
 }
 
-export default NavBar
+export default NavBar;
